1fbca1445f05d4f29ee2b8f1feda8845dc6c1e4a
LaTanya Donaldson <ldonald234@gmail.com>
2017-06-20 10:14:41 -0700

    Add last spec to js. Make html more detail

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 var leapYear = function(year) {
 
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   }
 
@@ -20,7 +20,18 @@ $(document).ready(function() {
 
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+    $(".year").text(year);
+
+    if (!result) { // same as writing if (result === false)
+      $(".not").text("not");
+    }
+
+    else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+
   });
 
 });
