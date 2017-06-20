1d30ace67bcc7155ec926a8e58963ec5019dbff0
LaTanya Donaldson <ldonald234@gmail.com>
2017-06-20 09:41:37 -0700

    We decided that our application should return false when the provided year is divisible by 100, because that means it is not a leap year. We'll add the smallest amount of code to create this behavior

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,12 @@
 
 var leapYear = function(year) {
 
-  if (year % 4 === 0) {
-    return true
+  if (year % 100 === 0) {
+    return false;
+  }
+
+  else if (year % 4 === 0) {
+    return true;
   }
 
   else {
